4549eb49e13828f8c533f4570836c995636c454d
Himan-Miku <himanshu28.mj@gmail.com>
2023-12-17 22:53:21 +0530

    feat: add basic express app

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,1 +1,15 @@
+import express from "express";
+import connectDB from "./config/database";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT ? process.env.PORT : 8000;
+
+connectDB();
+
+app.listen(PORT, () => {
+  console.log(`Server running on PORT:${PORT}`);
+});
+
 console.log("Basement is empty 💀");
