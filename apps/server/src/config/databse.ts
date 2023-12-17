3121b8323e68c84f70ff1caa194b66b6b58b3ba6
Himan-Miku <himanshu28.mj@gmail.com>
2023-12-17 13:37:51 +0530

    feat(db): add db config

---
+++ apps/server/src/config/databse.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI!, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+  }
+};
+
+export default connectDB;
