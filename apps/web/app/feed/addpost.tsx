098b6d8b76834da9ac29203f4b8cc520e08a9253
Himanshu Jagdale <himanshu28.mj@gmail.com>
2024-01-03 18:22:52 +0530

    feat: add createPost functionality

--- apps/web/app/feed/addpost.tsx
+++ apps/web/app/feed/addpost.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { Avatar, AvatarImage, AvatarFallback } from "components/ui/avatar";
 import { Button } from "components/ui/button";
-// import { Input } from "components/ui/input";
 import { ChangeEvent, useRef, useState } from "react";
 import {
   Dialog,
