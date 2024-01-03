27e23105362d4ee4805fa458b6b38ba01851b352
Himanshu Jagdale <himanshu28.mj@gmail.com>
2024-01-03 18:22:33 +0530

    feat: createPost functionality

--- apps/web/app/feed/addpost.tsx
+++ apps/web/app/feed/addpost.tsx
@@ -1,67 +1,230 @@
 "use client";
 import { Avatar, AvatarImage, AvatarFallback } from "components/ui/avatar";
 import { Button } from "components/ui/button";
-import { Input } from "components/ui/input";
-import { ChangeEvent, useRef } from "react";
-import Article from "public/Article.svg";
-import "../globals.css";
-
-import Event from "public/Event.svg";
+// import { Input } from "components/ui/input";
+import { ChangeEvent, useRef, useState } from "react";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "components/ui/dialog";
+import ReactTextareaAutosize from "react-textarea-autosize";
 import Media from "public/media.svg";
 import Image from "next/image";
 
+type receivedMetadata = {
+  putUri: string;
+  user: string;
+  objectKey: string;
+};
+
+type receivedGetUri = {
+  getUri: string;
+};
+
 const AddPost = () => {
   const fileInput = useRef<HTMLInputElement>(null);
-
+  const [media, setMedia] = useState<string | null>(null);
+  const [fileToUpload, setFileToUpload] = useState<File | null>(null);
+  const [filetype, setFiletype] = useState<string | null>(null);
+  const [description, setDescription] = useState("");
   const handleButtonClick = () => {
     fileInput.current && fileInput.current.click();
   };
 
-  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+  const handleFileChange = async (event: ChangeEvent<HTMLInputElement>) => {
     if (event.target.files && event.target.files.length > 0) {
-      const file = event.target.files[0];
-      console.log(file);
+      const media = event.target.files[0];
+      setFileToUpload(media!);
+      console.log(media);
+
+      if (media?.type.startsWith("image")) {
+        setFiletype("image");
+      } else if (media?.type.startsWith("video")) {
+        setFiletype("video");
+      } else {
+        setFiletype(null);
+      }
+
+      setMedia(URL.createObjectURL(media!));
     }
   };
+
+  const removeMediaFiles = () => {
+    if (media) {
+      setMedia(null);
+    }
+  };
+
+  const handleTextChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    setDescription(event.target.value);
+  };
+
+  const handleSubmit = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+
+    const metadata = {
+      user: "Himanshu",
+      size: fileToUpload?.size,
+      name: fileToUpload?.name,
+      contentType: fileToUpload?.type,
+    };
+
+    try {
+      const response = await fetch("http://localhost:8000/api/metadata", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(metadata),
+      });
+
+      if (response.ok) {
+        const uriData = (await response.json()) as receivedMetadata;
+        console.log("uriData: ", uriData);
+
+        try {
+          const res = await fetch(uriData.putUri, {
+            method: "PUT",
+            headers: {
+              "Content-Type": metadata.contentType!,
+            },
+            body: fileToUpload,
+          });
+
+          if (res.ok) {
+            console.log("File uploaded to s3 successfully");
+            console.log("Media put url : ", res.url);
+
+            const folderName = uriData.objectKey.split("/")[0];
+            const actualKey = uriData.objectKey.split("/")[1];
+
+            try {
+              const mediaRes = await fetch(
+                `http://localhost:8000/api/metadata?folderName=${folderName}&objectKey=${actualKey}`,
+                {
+                  method: "GET",
+                }
+              );
+
+              const data = (await mediaRes.json()) as receivedGetUri;
+              console.log("Get URI: ", data.getUri);
+
+              const dataToSend = {
+                user: metadata.user,
+                mediaUrl: data.getUri,
+                description,
+              };
+
+              try {
+                const postRes = await fetch("http://localhost:8000/api/post", {
+                  method: "POST",
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                  body: JSON.stringify(dataToSend),
+                });
+
+                const data = await postRes.json();
+
+                console.log(data);
+              } catch (error) {
+                console.log("Error while sending url to backend", error);
+              }
+            } catch (error) {
+              console.log("Error while getting media URL: ", error);
+            }
+          } else {
+            console.error("Error while uploading: ", res.text);
+          }
+        } catch (error) {
+          console.log("Error while posting media with putsignurl: ", error);
+        }
+      } else {
+        console.log("Error in response from metadata: ", response.text);
+      }
+    } catch (error) {
+      console.log("Error while sending Metadata: ", error);
+    }
+  };
+
   return (
-    <div className=" lg:col-span-3">
-      <div className="flex p-4 gap-4 items-start bg-white shadow-md rounded-md">
-        <Avatar className="w-12 h-12">
-          <AvatarImage
-            src="https://avatars.githubusercontent.com/u/127422698?s=96&v=4"
-            alt="DP"
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline">Create a Post</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[525px]">
+        <DialogHeader>
+          <DialogTitle>
+            <div className="flex gap-3 items-center">
+              <Avatar className="w-12 h-12">
+                <AvatarImage
+                  src="https://avatars.githubusercontent.com/u/127422698?s=96&v=4"
+                  alt="DP"
+                />
+                <AvatarFallback>SR</AvatarFallback>
+              </Avatar>
+              <div className="flex flex-col">
+                <h3 className="font-semibold text-base">Sushant Rao</h3>
+                <h3 className="font-medium text-sm">About Information</h3>
+              </div>
+            </div>
+          </DialogTitle>
+        </DialogHeader>
+        <div className="py-2 flex flex-col gap-2">
+          <ReactTextareaAutosize
+            minRows={4}
+            maxRows={10}
+            placeholder="What do you want to talk about?"
+            className="w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+            onChange={handleTextChange}
+            value={description}
           />
-          <AvatarFallback>SR</AvatarFallback>
-        </Avatar>
-        <div className="flex flex-col gap-2">
-          <Input placeholder="Start a post" className="w-full" />
-          <div className="flex gap-3">
-            <input
-              type="file"
-              style={{ display: "none" }}
-              ref={fileInput}
-              onChange={handleFileChange}
-            />
-            <Button
-              variant={"ghost"}
-              className="flex gap-2"
-              onClick={handleButtonClick}
-            >
-              <Image src={Media} alt="media"></Image>
-              <span>Media</span>
-            </Button>
-            <Button variant={"ghost"} className="flex gap-2">
-              <Image src={Event} alt="event"></Image>
-              <span>Event</span>
-            </Button>
-            <Button variant={"ghost"} className="flex gap-2">
-              <Image src={Article} alt="article"></Image>
-              <span>Article</span>
-            </Button>
-          </div>
+          {media && filetype === "image" ? (
+            <Image
+              src={media!}
+              alt="uploaded image"
+              width={200}
+              height={100}
+            ></Image>
+          ) : filetype === "video" ? (
+            <iframe src={media!} className="aspect-video"></iframe>
+          ) : (
+            <></>
+          )}
         </div>
-      </div>
-    </div>
+        <DialogFooter className="justify-center">
+          <input
+            type="file"
+            style={{ display: "none" }}
+            ref={fileInput}
+            onChange={handleFileChange}
+          />
+          <DialogClose asChild>
+            <Button type="button" onClick={removeMediaFiles}>
+              Close
+            </Button>
+          </DialogClose>
+          <Button
+            variant={"ghost"}
+            className="flex gap-2"
+            onClick={handleButtonClick}
+          >
+            <Image src={Media} alt="media"></Image>
+            <span>Media</span>
+          </Button>
+          <Button onClick={handleSubmit} type="submit">
+            Create
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 };
 
