ed10898130cad0b5cf59edd4d7f4958b22b4ab8d
Himanshu Jagdale <himanshu28.mj@gmail.com>
2024-01-03 18:10:35 +0530

    feat: add cors origin & metadataRoutes

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -2,14 +2,21 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import userRoutes from "./routes/userRoute";
 import postRoutes from "./routes/postRoute";
+import metadataRoutes from "./routes/metadataRoute";
+import cors from "cors";
 
 const app = express();
 //cookie body and encoded url configurations
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 
 //defining all routes here
-app.use("/api", userRoutes, postRoutes);
+app.use("/api", userRoutes, postRoutes, metadataRoutes);
 
 export default app;
