530ea3885b84ad807836f7abfc18798a5553fc82
Himanshu Jagdale <himanshu28.mj@gmail.com>
2024-01-08 20:20:32 +0530

    perf: add conditional logic for mongo connection

--- apps/web/lib/Connection.ts
+++ apps/web/lib/Connection.ts
@@ -1,15 +1,16 @@
 /* eslint-disable turbo/no-undeclared-env-vars */
-"use server"
+"use server";
 
 import mongoose, { MongooseError } from "mongoose";
 
 const connectDB = async (): Promise<void> => {
+  const mongoUrl =
+    process.env.NODE_ENV === "production"
+      ? process.env.MONGODB_PRODUCTION_URI!
+      : `mongodb://root:password@localhost:27017/${process.env.DB_NAME}`;
+
   try {
-    if(!process.env.MONGODB_URI){
-        throw new Error("MONGODB_URI not found")
-    }
-    
-    const mongoConnection = await mongoose.connect(process.env.MONGODB_URI);
+    const mongoConnection = await mongoose.connect(mongoUrl);
     console.log(`Mongo running at : ${mongoConnection.connection.name}`);
   } catch (error) {
     if (error instanceof MongooseError) {
