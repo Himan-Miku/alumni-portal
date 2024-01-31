1b45d8be8a5d38d973e4d785897427550508918a
Sushant Rao <127422698+sushant71rao@users.noreply.github.com>
2024-01-31 23:18:16 +0530

    Update Axios.ts

--- apps/web/app/Axios.ts
+++ apps/web/app/Axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const Axios = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACKEND,
-  headers:{"Custom header":"Sushant"}
+  headers:{"Authorization":"Bearer eyjh66272766vs6v"}
 });
 
 export default Axios;
