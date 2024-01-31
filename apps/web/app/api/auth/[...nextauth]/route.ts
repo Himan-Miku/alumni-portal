ed85f9f6a39379cbf591c103d7d2e999e99f69cd
Sushant Rao <127422698+sushant71rao@users.noreply.github.com>
2024-01-31 23:02:12 +0530

    Update route.ts

--- apps/web/app/api/auth/[...nextauth]/route.ts
+++ apps/web/app/api/auth/[...nextauth]/route.ts
@@ -86,6 +86,7 @@ const handler = NextAuth({
       options: {
         httpOnly: true,
         sameSite: "none",
+        domain:"https://alumni-backend-main.onrender.com",
         path: "/",
         secure: true,
       },
