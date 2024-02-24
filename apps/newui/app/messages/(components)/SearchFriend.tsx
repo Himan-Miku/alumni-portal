8fe5ccc930352e93b5f2875c9460aebf2b2a0f3e
Himanshu Jagdale <himanshu28.mj@gmail.com>
2024-02-24 19:02:21 +0530

    style: 🖌️add search input in messageslist compo

---
+++ apps/newui/app/messages/(components)/SearchFriend.tsx
@@ -0,0 +1,11 @@
+export default function SearchFriend() {
+  return (
+    <>
+      <input
+        className="bg-[#F5F5F5] rounded-md px-2 py-1 w-[90%] ml-3"
+        type="text"
+        placeholder="zara larson"
+      />
+    </>
+  );
+}
